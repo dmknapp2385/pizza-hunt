99038c792f1b21cd1d523dfc7d4c95d729473eb9
dmknapp2385 <dmknapp2385@gmail.com>
2022-03-02 11:08:57 -0700

    reply model and indexdb added

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -9,7 +9,7 @@ const commentController = {
             return Pizza.findOneAndUpdate(
                 { _id: params.pizzaId }, 
                 { $push: { comments: _id } },
-                { new: true }
+                { new: true, runValidators: true }
             );
         })
         .then(dbPizzaData => {
@@ -19,6 +19,33 @@ const commentController = {
             }
             res.json(dbPizzaData);
         })
+        .catch(err => res.json(err));
+    },
+
+    addReply({ params, body }, res) {
+        Comment.findOneAndUpdate(
+            { _id: params.commentId }, 
+            { $push: { replies: body } },
+            { new: true , runValidators: true}       
+        )
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this id!' });
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.json(err));
+    },
+
+    // remove reply
+    removeReply({ params }, res) {
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $pull: { replies: {replyId: params.replyId } } },
+            { new: true , runValidators: true}
+        )
+        .then(dbPizzaData => res.json(dbPizzaData))
         .catch(err => res.json(err));
     },
 
